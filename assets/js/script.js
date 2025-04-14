fd2bbb54cc6bc0d88ac7d55e14c7c827b227ea6f
Dante Pentito <8910dante@gmail.com>
2025-04-14 12:34:47 -0300

    Added expandable images
    Se edito style, script e Index para permitir ampliacion de imagenes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,26 @@ window.onscroll = () => {
     })
 }
 
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+document.querySelectorAll('.expandable').forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+    });
+});
+
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
